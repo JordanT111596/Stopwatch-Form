fb0f1f06d89a1b81340774e0cf54c6a3911c9d7f
Jordan Triplett <JordanT111596@gmail.com>
2021-03-02 15:22:16 -0500

    Form components created

--- Stopwatch-Form/Form1.Designer.cs
+++ Stopwatch-Form/Form1.Designer.cs
@@ -29,13 +29,140 @@ namespace Stopwatch_Form
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.minuteLabel = new System.Windows.Forms.Label();
+            this.secondLabel = new System.Windows.Forms.Label();
+            this.miliLabel = new System.Windows.Forms.Label();
+            this.colonLabel = new System.Windows.Forms.Label();
+            this.dotLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // minuteLabel
+            // 
+            this.minuteLabel.AutoSize = true;
+            this.minuteLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minuteLabel.Location = new System.Drawing.Point(395, 216);
+            this.minuteLabel.Name = "minuteLabel";
+            this.minuteLabel.Size = new System.Drawing.Size(127, 91);
+            this.minuteLabel.TabIndex = 0;
+            this.minuteLabel.Text = "00";
+            // 
+            // secondLabel
+            // 
+            this.secondLabel.AutoSize = true;
+            this.secondLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondLabel.Location = new System.Drawing.Point(528, 216);
+            this.secondLabel.Name = "secondLabel";
+            this.secondLabel.Size = new System.Drawing.Size(127, 91);
+            this.secondLabel.TabIndex = 1;
+            this.secondLabel.Text = "00";
+            // 
+            // miliLabel
+            // 
+            this.miliLabel.AutoSize = true;
+            this.miliLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.miliLabel.Location = new System.Drawing.Point(676, 243);
+            this.miliLabel.Name = "miliLabel";
+            this.miliLabel.Size = new System.Drawing.Size(81, 58);
+            this.miliLabel.TabIndex = 2;
+            this.miliLabel.Text = "00";
+            // 
+            // colonLabel
+            // 
+            this.colonLabel.AutoSize = true;
+            this.colonLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colonLabel.Location = new System.Drawing.Point(496, 225);
+            this.colonLabel.Name = "colonLabel";
+            this.colonLabel.Size = new System.Drawing.Size(47, 69);
+            this.colonLabel.TabIndex = 3;
+            this.colonLabel.Text = ":";
+            // 
+            // dotLabel
+            // 
+            this.dotLabel.AutoSize = true;
+            this.dotLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dotLabel.Location = new System.Drawing.Point(633, 234);
+            this.dotLabel.Name = "dotLabel";
+            this.dotLabel.Size = new System.Drawing.Size(47, 69);
+            this.dotLabel.TabIndex = 4;
+            this.dotLabel.Text = ".";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.titleLabel.Location = new System.Drawing.Point(270, 87);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(675, 69);
+            this.titleLabel.TabIndex = 5;
+            this.titleLabel.Text = "Jordan\'s C# StopWatch";
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(282, 381);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(204, 93);
+            this.startButton.TabIndex = 6;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // stopButton
+            // 
+            this.stopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopButton.Location = new System.Drawing.Point(517, 381);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(204, 93);
+            this.stopButton.TabIndex = 7;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(741, 381);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(204, 93);
+            this.resetButton.TabIndex = 8;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1239, 854);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.dotLabel);
+            this.Controls.Add(this.colonLabel);
+            this.Controls.Add(this.miliLabel);
+            this.Controls.Add(this.secondLabel);
+            this.Controls.Add(this.minuteLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label minuteLabel;
+        private System.Windows.Forms.Label secondLabel;
+        private System.Windows.Forms.Label miliLabel;
+        private System.Windows.Forms.Label colonLabel;
+        private System.Windows.Forms.Label dotLabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button resetButton;
     }
 }
 
